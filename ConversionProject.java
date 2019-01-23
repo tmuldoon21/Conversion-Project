ae1c81825f4be2a7858bb92030cda3fc2fb3aa53
tmuldoon21 <42975519+tmuldoon21@users.noreply.github.com>
2019-01-23 12:42:46 -0500

    Add files via upload

--- ConversionProject.java
+++ ConversionProject.java
@@ -1,9 +1,31 @@
+/**
+ * 
+ * @author Tyler Muldoon
+ * Conversion Project
+ *
+ */
+public class ConversionProject 
+{
 
-public class ConversionProject {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
 
 	}
-
+	
+	public static float feetToInchees()
+	{
+		
+	}
+	
+	public static float milesToFeet()
+	{
+		
+	}
+	public static float yardsToMeters()
+	{
+		
+	}
+	
+	public static float poundsToKilograms()
 }
